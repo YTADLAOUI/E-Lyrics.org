0047adf41ba8901af142caa089d75deae8cf6d66
youssef tadlaoui <usftadlaoui@gmail.com>
2023-01-16 20:46:04 +0100

    navbar

---
+++ accuile.php
@@ -0,0 +1,22 @@
+<?php include './includes/hedear.php';?>
+<body>
+  
+<nav class="navbar bg-danger">
+  <div class="container-fluid">
+    <a class="navbar-brand">E-Lyrics</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+<div class="sidebar">
+   
+</div>
+<main>
+
+</main>
+
+
+</body>
+</html>
